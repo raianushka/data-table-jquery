028b9fb81be86aacbabb2352f15a8666f976ed17
raianushka <96331163+raianushka@users.noreply.github.com>
2024-01-29 16:54:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -12,38 +12,103 @@ $(document).ready(function() {
         { data: 'version' },
         {
           "data": null,
-          "render": function(data, type, row) {
+          "render": function(row) {
                     return'<button id="delete-btn" onclick="deleteRecord(' + row.id + ')">Delete</button>';
           }
         }
       ]
     });
-     var table = $('#myTable').DataTable();
- 
+
+    var table = $('#myTable').DataTable();
     $('#myTable tbody').on( 'click', '#delete-btn', function () {
-    table
-      .row( $(this).parents('tr') )
-      .remove()
-      .draw();
-    });
      
-    $('#myTable tbody').on('click', 'td', function () {
-      var cell = table.cell(this);
-      var columnIndex = cell.index().column;
-      var rowIndex = cell.index().row;
+        let text = "Are you sure you want to delete this row?";
+        if (confirm(text) == true) {
+          table
+          .row( $(this).parents('tr') )
+          .remove()
+          .draw();
+        } else {
+          alert("You canceled!");
+        }
+       
+    });
 
-      // Check if the clicked cell is editable (modify this condition based on your needs)
-      if (columnIndex !== 0) { // Example: Allow editing all columns except the first one (ID)
-          var currentData = cell.data();
-          var inputType = columnIndex === 3 ? 'number' : 'text'; // Assuming the last column is numeric
+           // Open the dialog on button click
+           $("#addRowBtn").on("click", function () {
+            $("#dialog-form").dialog("open");
+        });
 
-          cell.data('<input type="' + inputType + '" class="inline-edit" value="' + currentData + '">').draw();
+        // Define the dialog
+        $("#dialog-form").dialog({
+            autoOpen: false,
+            height: 400,
+            width: 450,
+            modal: true,
+            buttons: {
+                "Add Row": function () {
+                    // Validate and add the new row
+                    addRow();
+                    $(this).dialog("close");
+                },
+                Cancel: function () {
+                    $(this).dialog("close");
+                }
+            },
+            close: function () {
+                // Reset the form on close
+                $("#addRowForm")[0].reset();
+            }
+        });
 
-          // Focus on the input field and bind blur event
-          $('.inline-edit').focus().blur(function () {
-              var newValue = $(this).val();
-              cell.data(newValue).draw();
-          });
-      }
+        // Event listener for form submission to add a new row
+        $("#addRowForm").submit(function (event) {
+            event.preventDefault();
+            addRow();
+            $("#dialog-form").dialog("close");
+        });
+
+        function addRow() {
+            // Generate a unique ID (you may use a function to generate IDs)
+            var newId = Date.now().toString();
+
+            // Get form values
+            var newName = $("#name").val();
+            var newLanguage = $("#language").val();
+            var newVersion = $("#version").val();
+
+            // Add a new row to the DataTable
+            var newRowData = {
+                id: newId,
+                name: newName,
+                language: newLanguage,
+                version: newVersion
+            };
+
+            table.row.add(newRowData).draw();
+        }
+
+        $('#myTable tbody').on('click', 'td', function () {
+          var cell = table.cell(this);
+          var columnIndex = cell.index().column;
+          var rowIndex = cell.index().row;
+    
+          // if (columnIndex !== 0) { //Allow editing all columns except the first one (ID)
+              var currentData = cell.data();
+              var inputType = columnIndex === 3 ? 'number' : 'text'; // Assuming the last column is numeric
+    
+              cell.data('<input type="' + inputType + '" class="inline-edit" value="' + currentData + '">').draw();
+    
+              // Focus on the input field and bind blur event
+              $('.inline-edit').focus().blur(function () {
+                var newValue = $(this).val();
+                let text = "Are you sure you want to edit this row?";
+                  if (confirm(text) == true) {
+                    cell.data(newValue).draw();
+               } else {
+               alert("You canceled!");
+                }
+                 
+              });
+            });
   });
-});
